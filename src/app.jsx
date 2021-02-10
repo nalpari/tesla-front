ded0de241409b63513086a271c765042e3081b54
nalpari <yoo32767@gmail.com>
2021-02-10 10:36:10 +0900

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './components/login/login';
+import Dashboard from './components/dashboard/dashboard';
+import styles from "./app.module.css";
+
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul className={styles.topMenu}>
+          <li>
+            <Link to="/">dashboard</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
---
+++ src/components/login/login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './login.module.css';
+
+const Login = () => {
+  return (
+    <h1>login</h1>
+  )
+};
+
+export default Login;
